038d6971232b0a914a257744a1d26f846336f694
Sergey <sergeyskoromnik@gmail.com>
2024-10-14 17:47:41 +0300

    Update gh-pages version

--- dist/js/app.js
+++ dist/js/app.js
@@ -199,6 +199,10 @@
             }
         }
     }
+    function functions_menuClose() {
+        bodyUnlock();
+        document.documentElement.classList.remove("menu-open");
+    }
     function functions_FLS(message) {
         setTimeout((() => {
             if (window.FLS) console.log(message);
@@ -3969,8 +3973,7 @@
             navigation: {
                 prevEl: ".swiper-button-prev",
                 nextEl: ".swiper-button-next"
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".directions-slider")) new swiper_core_Swiper(".directions-slider", {
             modules: [ Navigation, Pagination ],
@@ -3988,8 +3991,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".service-slider")) new swiper_core_Swiper(".service-slider", {
             modules: [ Navigation, Pagination ],
@@ -4007,8 +4009,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".photo-slider")) new swiper_core_Swiper(".photo-slider", {
             modules: [ Navigation, Pagination ],
@@ -4026,8 +4027,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".reviews-slider")) new swiper_core_Swiper(".reviews-slider", {
             modules: [ Navigation, Pagination ],
@@ -4045,8 +4045,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".employees-slider")) new swiper_core_Swiper(".employees-slider", {
             modules: [ Navigation, Pagination ],
@@ -4064,8 +4063,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".partners-slider")) new swiper_core_Swiper(".partners-slider", {
             modules: [ Navigation, Pagination ],
@@ -4083,8 +4081,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".news-slider")) new swiper_core_Swiper(".news-slider", {
             modules: [ Navigation, Pagination ],
@@ -4102,8 +4099,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".share-slider")) new swiper_core_Swiper(".share-slider", {
             modules: [ Navigation, Pagination ],
@@ -4121,8 +4117,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".instagram-slider")) new swiper_core_Swiper(".instagram-slider", {
             modules: [ Navigation, Pagination ],
@@ -4140,8 +4135,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".teaser-slider")) new swiper_core_Swiper(".teaser-slider", {
             modules: [ Navigation, Pagination ],
@@ -4159,8 +4153,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".projects-slider")) new swiper_core_Swiper(".projects-slider", {
             modules: [ Navigation, Pagination ],
@@ -4178,8 +4171,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".categories-slider")) new swiper_core_Swiper(".categories-slider", {
             modules: [ Navigation, Pagination ],
@@ -4197,8 +4189,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".product-slider")) new swiper_core_Swiper(".product-slider", {
             modules: [ Navigation, Pagination ],
@@ -4216,8 +4207,7 @@
                 992: {
                     spaceBetween: 20
                 }
-            },
-            on: {}
+            }
         });
         if (document.querySelector(".page-slider")) new swiper_core_Swiper(".page-slider", {
             modules: [ Navigation, Pagination ],
@@ -7010,6 +7000,206 @@ PERFORMANCE OF THIS SOFTWARE.
             });
         }));
         modules_flsModules.gallery = galleyItems;
+    }
+    const header = document.querySelector("header");
+    const dropButton = document.querySelectorAll(".menu__svg");
+    const mobileBackButtons = document.querySelectorAll(".mobile-back");
+    const contactsPhoneButton = document.querySelectorAll(".contacts-phone__arrow");
+    const asidePhonesArrow = document.querySelectorAll(".aside-phone__arrow");
+    const asideDropButtons = document.querySelectorAll(".aside-menu__svg");
+    const asideCloseButton = document.querySelector(".aside-menu__close");
+    const asideMenuOverlay = document.querySelector(".aside-menu__overlay");
+    const pageMenuButtons = document.querySelectorAll(".page-menu__svg");
+    function closeMenu() {
+        bodyUnlock();
+        document.documentElement.classList.remove("menu-open");
+    }
+    function closeAsideMenu() {
+        asideCloseButton.addEventListener("click", (() => {
+            functions_menuClose();
+        }));
+    }
+    closeAsideMenu();
+    function dropMenu(array, selector, event = "click") {
+        array.forEach((item => {
+            item.addEventListener(`${event}`, (e => {
+                e.preventDefault();
+                const target = e.currentTarget;
+                const parentDrop = target.closest(selector);
+                parentDrop.classList.add("drop");
+                header.classList.add("drop");
+            }));
+        }));
+    }
+    dropMenu(dropButton, ".menu__item", "touchstart");
+    dropMenu(asideDropButtons, ".aside-menu__item");
+    dropMenu(contactsPhoneButton, ".contacts-phone");
+    dropMenu(asidePhonesArrow, ".aside-phone");
+    function script_removeDrop(array, selector) {
+        array.forEach((item => {
+            item.addEventListener("click", (e => {
+                e.preventDefault();
+                const target = e.currentTarget;
+                const parentDrop = target.closest(".drop");
+                if (parentDrop.matches(`${selector} .drop`)) parentDrop.classList.remove("drop"); else {
+                    header.classList.remove("drop");
+                    parentDrop.classList.remove("drop");
+                }
+            }));
+        }));
+    }
+    script_removeDrop(mobileBackButtons, ".menu__item");
+    function removeSomeDrop(selector) {
+        const allDrop = document.querySelectorAll(selector);
+        allDrop.forEach((item => {
+            item.classList.remove("drop");
+        }));
+    }
+    asideMenuOverlay.addEventListener("click", (() => {
+        closeMenu();
+        removeSomeDrop(".aside-phone");
+        removeSomeDrop(".aside-menu__item");
+    }));
+    document.addEventListener("click", ((e, selector) => {
+        if (e.target.closest(`${selector}`)) removeSomeDrop(`${selector}`);
+    }));
+    document.addEventListener("keydown", (e => {
+        if (e.key === "Escape") {
+            closeMenu();
+            removeSomeDrop(".aside-phone");
+            removeSomeDrop(".contacts-phone");
+        }
+    }));
+    function toggleBtnForm() {
+        const checkBoxes = document.querySelectorAll("[data-formCheckbox]");
+        checkBoxes.forEach((item => {
+            item.addEventListener("change", (e => {
+                const currTarget = e.currentTarget;
+                const parentElement = currTarget.closest("[data-form]");
+                const currBtn = parentElement.querySelector("[data-formBtn] button");
+                const currCheck = item.querySelector("input");
+                const isChecked = currCheck.checked;
+                toggleBtnAttr(isChecked, currBtn);
+            }));
+        }));
+    }
+    function setBtnState() {
+        const forms = document.querySelectorAll("[data-form]");
+        forms.forEach((form => {
+            const formCheckBox = form.querySelector("[data-formCheckbox] input");
+            const formBtn = form.querySelector("[data-formBtn] button");
+            const isChecked = formCheckBox.checked;
+            toggleBtnAttr(isChecked, formBtn);
+        }));
+    }
+    function toggleBtnAttr(state, btn) {
+        if (state) btn.removeAttribute("disabled"); else btn.setAttribute("disabled", "");
+    }
+    if (document.querySelector("[data-formCheckbox]")) {
+        setBtnState();
+        toggleBtnForm();
+    }
+    if (document.querySelector(".count")) {
+        const countsBlocks = document.querySelectorAll(".count");
+        countsBlocks.forEach((block => {
+            block.addEventListener("click", (e => {
+                const target = e.target;
+                if (target.closest(".count__minus")) {
+                    const counter = target.closest(".count").querySelector(".count__counter");
+                    +counter.textContent > 1 ? counter.textContent-- : counter.textContent;
+                }
+                if (target.closest(".count__plus")) {
+                    target.closest(".count").querySelector(".count__counter").textContent++;
+                }
+            }));
+        }));
+    }
+    if (document.querySelector(".menu__list")) {
+        hideMenuItem();
+        window.addEventListener("resize", (() => {
+            hideMenuItem();
+        }));
+    }
+    function hideMenuItem() {
+        const menus = document.querySelectorAll("[data-menu]");
+        for (let menu of menus) {
+            const menuList = menu.querySelector(".menu__list");
+            const menuListChildren = menuList.children;
+            const menuLastItem = menuList.querySelector("[data-menu-group]");
+            const menuLastItemList = menuLastItem.querySelector(".menu__list");
+            const menuLastItemListChildren = menuLastItemList.children;
+            const styles = window.getComputedStyle(menuList, null);
+            const gap = +styles.rowGap.slice(0, -2);
+            let width = menuList.offsetWidth;
+            let totalWidthItem = 0;
+            for (let item of menuListChildren) totalWidthItem += item.clientWidth;
+            totalWidthItem += gap * menuListChildren.length - 1;
+            if (totalWidthItem > width && menuListChildren.length > 2) {
+                menuRemoveChild(menuList, menuListChildren, menuLastItemList);
+                hideMenuItem();
+            }
+            if (totalWidthItem + 110 < width && menuListChildren.length < 9 && menuLastItemListChildren.length > 1) menuBackItem(menuList, menuLastItemList, menuLastItemListChildren);
+        }
+    }
+    function menuRemoveChild(menuList, menuListChildren, menuLastItemList) {
+        let element = menuList.removeChild(menuListChildren[menuListChildren.length - 2]);
+        menuLastItemList.appendChild(element);
+    }
+    function menuBackItem(menuList, menuLastItemList, menuLastItemListChildren) {
+        let element = menuLastItemList.removeChild(menuLastItemListChildren[menuLastItemListChildren.length - 1]);
+        menuList.insertBefore(element, menuList.lastElementChild);
+    }
+    //! page menu drop
+        function dropPageMenu(array, selector, event = "click") {
+        array.forEach((item => {
+            item.addEventListener(`${event}`, (e => {
+                e.preventDefault();
+                const target = e.currentTarget;
+                const parentDrop = target.closest(selector);
+                parentDrop.classList.toggle("drop");
+            }));
+        }));
+    }
+    if (document.querySelector(".page-menu")) dropPageMenu(pageMenuButtons, ".page-menu__item");
+    function eventAccord() {
+        const accordingAll = document.querySelectorAll(".accordion");
+        accordingAll.forEach((accordion => {
+            const accordionItems = accordion.children;
+            for (let item of accordionItems) item.addEventListener("click", (e => {
+                const targetChild = e.currentTarget;
+                if (targetChild.matches(".drop")) targetChild.classList.toggle("drop"); else {
+                    for (let item of accordionItems) item.classList.remove("drop");
+                    targetChild.classList.toggle("drop");
+                }
+            }));
+        }));
+    }
+    if (document.querySelector(".accordion")) eventAccord();
+    if (document.querySelector(".upload__button")) uploadFileInForm();
+    function uploadFileInForm() {
+        const popupUploadButtons = document.querySelectorAll(".upload__button");
+        popupUploadButtons.forEach((button => {
+            button.addEventListener("click", (e => {
+                e.preventDefault();
+                const target = e.currentTarget;
+                const inputTarget = target.parentElement.previousElementSibling;
+                inputTarget.click();
+                inputTarget.addEventListener("change", (e => {
+                    let currentFileName = "";
+                    const spanText = e.currentTarget.nextElementSibling.querySelector("span");
+                    if (e.currentTarget.files.length > 0) {
+                        const fileName = e.currentTarget.files[0].name;
+                        currentFileName = fileName.split(".")[0];
+                        spanText.textContent = "";
+                        spanText.textContent = `${currentFileName.slice(0, 22)}...`;
+                    } else {
+                        spanText.textContent = "";
+                        currentFileName = "Файл не выбран";
+                        spanText.textContent = `${currentFileName}`;
+                    }
+                }));
+            }));
+        }));
     }
     window["FLS"] = true;
     tabs();
