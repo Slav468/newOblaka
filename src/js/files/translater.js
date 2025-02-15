88b78a3dfddbde0c7c767f01e7ba181468dd0307
Slav468 <55102248+Slav468@users.noreply.github.com>
2025-02-15 10:33:58 +0300

    Update gh-pages version

--- src/js/files/translater.js
+++
@@ -1,5 +0,0 @@
-import { languages, translate } from 'google-translate-api-x';
-
-const res = await translate('Я пишу Привет мир и перевожу', { to: 'be' });
-console.log(res.text); //=> I speak English
-console.log(languages); //=> nl
